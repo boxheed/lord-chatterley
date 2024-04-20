7aa0e13bd32391a3a75038c6f3b70fd27d7f5f71
Andrew Dunn <boxheed@users.noreply.github.com>
2024-04-20 08:03:58 +0000

    feat added the API endpoint to run the llm

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -1,3 +1,20 @@
-export async function GET({ params, request }) {
-    return new Response("Hello, world!")
+export async function POST({ locals, params, request }) {
+    const runtime = locals.runtime;
+    const env = runtime.env;
+    const ai = env.AI;
+
+    const body = await request.text();
+    const payload = JSON.parse(body);
+
+    let question = {
+        messages: [
+          { role: 'system', content: 'You are a helpful assistant.' },
+          { role: 'user', content: payload.prompt }
+        ]
+      };
+  
+    const answer = await ai.run('@cf/meta/llama-2-7b-chat-int8', question);
+  
+    return Response.json(answer);
 }
+
