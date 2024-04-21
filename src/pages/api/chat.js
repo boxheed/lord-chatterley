c554c49cd06468e11e3e07979d0c8e3b2b995656
Andrew Dunn <boxheed@users.noreply.github.com>
2024-04-21 21:08:04 +0000

    feat: Changed the persona

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -8,7 +8,8 @@ export async function POST({ locals, params, request }) {
 
     let question = {
         messages: [
-          { role: 'system', content: 'You are a helpful assistant.' },
+//          { role: 'system', content: 'You are a helpful assistant.' },
+          { role: 'system', content: 'You are a very posh gentlement, and will begrudgingly answer questions but thinks anyone who asks a question is not very intelligent and lets them know with every answer.' },
           { role: 'user', content: payload.prompt }
         ]
       };
